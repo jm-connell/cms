b2e498d7160bc1394b85e1110d4d112fa227d1d1
jm-connell <con18013@byui.edu>
2025-05-13 19:13:43 -0600

    add contact-detail component

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
   standalone: true,
   imports: [],
   templateUrl: './contact-detail.component.html',
-  styleUrl: './contact-detail.component.css'
+  styleUrl: './contact-detail.component.css',
 })
 export class ContactDetailComponent {
-
+  contact: Contact = {
+    id: '1',
+    name: 'Example Contact',
+    email: 'contact@example.com',
+    phone: '555-555-5555',
+    imageUrl: 'https://via.placeholder.com/80',
+    group: 'null',
+  };
 }
